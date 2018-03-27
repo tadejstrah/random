acce1c99869864f5dba9e4051755aef93703163f
tadejstrah <tadej.strah@gmail.com>
2018-03-27 15:38:03 +0200

    sort

--- programerski/quick_sort_algoritem/quick_sort.py
+++ programerski/quick_sort_algoritem/quick_sort.py
@@ -1,6 +1,27 @@
+import random
 
-arr = [1,2,6,3,7,2,5,3,4,7]
+arr = [3,2,6,3,7,2,5,3,4,7]
+
+for x in range(1000000):
+    arr.append(random.randint(0,1000000))
+
+print("zacel")
 
 def sort(arraycek):
-    
+    if len(arraycek) <= 1:
+        return arraycek
+    pivot = arraycek[0]
+    arr1 = []
+    arr2 = []
+    for i in range(1,len(arraycek)):
+        if arraycek[i] <= pivot:
+            arr1.append(arraycek[i])
+        else:
+            arr2.append(arraycek[i])
+    arr1 = sort(arr1)
+    arr2 = sort(arr2)
+    return arr1 + [pivot] + arr2
 
+#print(arr)
+#print(sort(arr))
+print("koncal")
