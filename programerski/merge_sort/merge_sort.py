93358e5e7b4cea83f7366f69555a075e0995d540
tadejstrah <tadej.strah@gmail.com>
2018-05-24 11:24:37 +0200

    neki

--- programerski/merge_sort/merge_sort.py
+++ programerski/merge_sort/merge_sort.py
@@ -1,15 +1,10 @@
 #import random
 
-
 arraycek = [4,2,67,3,5,3,43,3,2,634,53,534,534,534,5,3453,45,3523,46,23654,2645,36,4565,43,643,34,523,5436,4320,0,-2,-3]
 
 #for x in range(10000000):
 #    arraycek.append(random.randint(0,10000000))
-
 #print("zacel")
-
-
-
 
 def split(arr):
     levi = arr[:(len(arr)//2)]
--- programerski/quick_sort_algoritem/quick_sort.py
+++ programerski/quick_sort_algoritem/quick_sort.py
@@ -1,11 +1,11 @@
 import random
 
-arr = [3,2,6,3,7,2,5,3,4,7]
+arr = [3,2,6,3,15,15,5,3,4,7]
 
-for x in range(1000000):
-    arr.append(random.randint(0,1000000))
+#for x in range(1000000):
+ #   arr.append(random.randint(0,1000000))
 
-print("zacel")
+#print("zacel")
 
 def sort(arraycek):
     if len(arraycek) <= 1:
@@ -23,5 +23,23 @@ def sort(arraycek):
     return arr1 + [pivot] + arr2
 
 #print(arr)
-#print(sort(arr))
-print("koncal")
+print(sort(arr))
+#print("koncal")
+
+def sort2(arraycek):
+    if len(arraycek) <= 1:
+        return arraycek
+    pivot = arraycek[0]
+    arr1 = []
+    arr2 = []
+    for i in arraycek[1:]:
+        if i <= pivot:
+            arr1.append(i)
+        else:
+            arr2.append(i)
+    arr1 = sort(arr1)
+    arr2 = sort(arr2)
+    return arr1 + [pivot] + arr2
+
+
+print(sort2(arr))
