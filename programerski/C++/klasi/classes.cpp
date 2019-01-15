a0ed2a239052af1626ea164e240cdb529ed6bff2
tadejstrah <tadej.strah@gmail.com>
2019-01-15 18:29:47 +0100

    žnj

--- programerski/C++/klasi/classes.cpp
+++ programerski/C++/klasi/classes.cpp
@@ -1,4 +1,6 @@
 #include<string>
+#include<iostream>
+
 
 using namespace std;
 
@@ -8,10 +10,35 @@ class Vozilo{
 
     public: Vozilo(string barva, double masa): barva(barva), masa(masa){}   // konstruktor
 
+    virtual void pobarvaj(string b){  //virtual pomeni, da funkcijo v child clasu redefinaš/povoziš. samo če je funkcija virtualna se bo klicala potem funckija od childa in ne od parent classa
+        barva = b;
+        cout << "barvo vozila sem spremenil na " << b <<"\n";
+    };
+
 };
 
 
+//v c++ vectorju maš lohka sam iste tipe (isto velike). Lohka pa to rešiš da nardiš vector pointerjou na objekte različnih tipov
+// std::vector <Vozilo*> vozila;
+//vozila.push_back(new Avto(4,1000,"črna"))
+
+class Avto : public Vozilo{
+    int st_vrat;
+    string barvaKoles;
+    public: Avto(int s,double m, string b) : Vozilo(b,m), st_vrat(s){}  //initat morš parent class
+
+    void pobarvaj(string b) override {  //override, ker overrida Vozilo::pobarvaj
+        Vozilo::pobarvaj(b);
+        barvaKoles = b;
+        cout << "barvo koles in barvo vozila sem spremenil na " << b << "\n";
+
+    }
+};
+
 
 int main(){
+
+    Avto avto = Avto(4,1000,"črna");
+    avto.pobarvaj("bela");
 
 }
